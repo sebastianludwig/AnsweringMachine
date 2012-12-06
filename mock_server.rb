8d7ad94393159d987f8d96ecd74e82c17259e283
Sebastian Ludwig <sebastian@lurado.de>
2012-12-07 00:26:46 +0100

    Basic functions working

--- mock_server.rb
+++ mock_server.rb
@@ -1,33 +1,78 @@
 
 #DataMapper::Logger.new(STDOUT, :debug)
-DataMapper::setup(:default, "sqlite3://#{File.dirname(File.expand_path(__FILE__))}/twplus.db")
+DataMapper::setup(:default, "sqlite3://#{File.dirname(File.expand_path(__FILE__))}/server.db")
 DataMapper::Model.raise_on_save_failure = true  # globally across all models
 
-class Request
+class Response
   include DataMapper::Resource
   property :id, Serial
   property :path, String, :index => :path
+  property :http_status, Integer, :default => 200
+  property :resend, Boolean, :default => false
+  #property :forward, String
+  #property :conent_type, String
+  property :body, String
+  property :requested_at, DateTime
+  
+  default_scope(:default).update(:order => [:requested_at.desc])
+  
+  def self.sent
+    all(:conditions => [ 'requested_at IS NOT NULL' ])
+  end
+  
+  def self.scheduled
+    all(:conditions => [ "requested_at IS NULL OR resend = 't'" ])
+  end
 end
 
 DataMapper.finalize
-Request.auto_upgrade!
+Response.auto_upgrade!
 
 class MockServer < Sinatra::Base
+  use Rack::MethodOverride
+  
   configure do
-    set :public, 'public'
+    set :public_folder, 'public'
     set :show_exceptions, settings.development?
   end
 
   get '/index' do
-    @requests = Request.all
+    @requests = Response.all(:fields => [:path], :unique => true).map { |r| r.path }
+    
+    @sent_responses = Response.sent
+    @scheduled_responses = Response.scheduled
     
     erb :index    
   end
   
+  delete '/response/:id' do |id|
+    response = Response.get(id)
+    response.destroy if response
+    
+    redirect '/index'
+  end
+  
+  post '/add' do
+    Response.create(:path => params[:path], 
+                    :body => params[:body], 
+                    :http_status => params[:http_status].to_i,
+                    :resend => (params[:resend] == '1'))
+    
+    redirect '/index'
+  end
+  
   get '/*' do
     path = params[:splat].first
-    request_parameters = params.delete_if { |k, v| ['splat', 'captures'].include? k }
+    parameters = params.delete_if { |k, v| ['splat', 'captures'].include? k }
     
-    request = Request.create(:path => path)
+    response = Response.scheduled.first(:path => path)
+    if !response
+      response = Response.create(:path => path, :body => 'default', :http_status => 200)
+    end
+    response.attributes = { :requested_at => Time.now }
+    response.save
+
+    status response.http_status
+    body response.body
   end
 end
