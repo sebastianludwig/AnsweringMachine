3ad66d495a67f9eb35d62cf7b90f82204c133f7a
Sebastian Ludwig <sebastian@lurado.de>
2012-12-09 04:21:34 +0100

    Wicked status code selection

--- helper/view_helper.rb
+++ helper/view_helper.rb
@@ -6,10 +6,60 @@ module ViewHelper
     return ''
   end
   
-  def truncate(text, length = 30, truncate_string = "...")
-      if text
-        l = length - truncate_string.length
-        (text.length > length ? text[0...l] + truncate_string :  text).to_s
-      end
-    end
+  def http_status_codes(group)
+    return [
+      { :code => 200, :name => "OK" },
+      { :code => 201, :name => "Created" },
+      { :code => 202, :name => "Accepted" },
+      { :code => 203, :name => "Non-Authoritative Information" },
+      { :code => 204, :name => "No Content" },
+      { :code => 205, :name => "Reset Content" },
+      { :code => 206, :name => "Partial Content" }
+    ] if group == :successful
+    
+    return [
+      { :code => 300, :name => "Multiple Choices" },
+      { :code => 301, :name => "Moved Permanently" },
+      { :code => 302, :name => "Found" },
+      { :code => 303, :name => "See Other" },
+      { :code => 304, :name => "Not Modified" },
+      { :code => 306, :name => "Switch Proxy" },
+      { :code => 307, :name => "Temporary Redirect" },
+      { :code => 308, :name => "Resume Incomplete" }
+    ] if group == :redirection
+    
+    return [
+      { :code => 400, :name => "Bad Request" },
+      { :code => 401, :name => "Unauthorized" },
+      { :code => 402, :name => "Payment Required" },
+      { :code => 403, :name => "Forbidden" },
+      { :code => 404, :name => "Not Found" },
+      { :code => 405, :name => "Method Not Allowed" },
+      { :code => 406, :name => "Not Acceptable" },
+      { :code => 407, :name => "Proxy Authentication Required" },
+      { :code => 408, :name => "Request Timeout" },
+      { :code => 409, :name => "Conflict" },
+      { :code => 410, :name => "Gone" },
+      { :code => 411, :name => "Length Required" },
+      { :code => 412, :name => "Precondition Failed" },
+      { :code => 413, :name => "Request Entity Too Large" },
+      { :code => 414, :name => "Request-URI Too Long" },
+      { :code => 415, :name => "Unsupported Media Type" },
+      { :code => 416, :name => "Requested Range Not Satisfiable" },
+      { :code => 417, :name => "Expectation Failed" },
+      { :code => 418, :name => "I'm a teapot" }
+    ] if group == :client_error
+    
+    return [
+      { :code => 500, :name => "Internal Server Error" },
+      { :code => 501, :name => "Not Implemented" },
+      { :code => 502, :name => "Bad Gateway" },
+      { :code => 503, :name => "Service Unavailable" },
+      { :code => 504, :name => "Gateway Timeout" },
+      { :code => 505, :name => "HTTP Version Not Supported" },
+      { :code => 511, :name => "Network Authentication Required" }
+    ] if group == :server_error
+    
+    return []
+  end
 end
--- public/js/application.js
+++ public/js/application.js
@@ -3,11 +3,37 @@ $('.show-body').click(function(event) {
 });
 
 $('.response-body').hide();
+$('[rel=tooltip]').tooltip();
 
-$('.controls .btn-group .btn').click(function(event) {
-  $('#http_status').attr('value', $(this).attr('value'));
-});
+$('.status_button').bind('activate', function(event, value) {
+  $(this).closest('.http_status').find('.active').removeClass('active')
+  $(this).toggleClass('active')
+  $(this).text(value)
+  $('#http_status').attr('value', value)
+})
 
+$('.status_button').click(function(event) {
+  $(this).trigger('activate', $(this).text().trim())
+})
+
+$('.http_status .dropdown-menu li a').click(function(event) {
+  $(this)
+    .closest('.btn-group')
+    .find('.status_button')
+    .trigger('activate', $(this).text().trim())
+})
+
+$('.http_status .dropdown-menu li > a').hover(
+  function() {
+    $(this).css('background', function(index, value) {
+      return $(this).closest('.btn-group').find('.dropdown_button').css('background')
+    })
+  },
+  function() {
+    $(this).css('background', 'white')
+  }
+)
+  
 $('.delete,.put').click(function() {
   var method = 'POST';
   if ($(this).hasClass('put')) {
